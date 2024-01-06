59b7638ea751fd0f798931c6df0d7a7d596a5d50
Jon Daries <jon.daries@gmail.com>
2024-01-05 23:02:37 -0800

    adds ability to pick a specific entry and footer

--- mg_receipt.py
+++ mg_receipt.py
@@ -8,6 +8,8 @@ from escpos import config
 
 LOGO = "logos/Memory-Gap-B-print.jpg"
 HEAD = "mg_receipt_header.txt"
+FOOT = "mg_receipt_footer.txt"
+QRLINK = "https://linktr.ee/madonno_productions"
 WIDTH = 42
 MALLS = "malls.csv"
 PHOTO_DIR = "./mall_photos"
@@ -16,12 +18,20 @@ STORY_DIR = "/mall_stories"
 c = config.Config()
 p = c.printer()
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--index', type=int,
+                    help="row number (zero-indexed starting from first row of data) of the malls.csv file to use, overrides the randomness")
+
 if __name__ == '__main__':
+    args = parser.parse_args()
     reader = csv.DictReader(open(MALLS,"r"))
     mall_db = list()
     for row in reader:
         mall_db.append(row)
-    mall = random.choice(mall_db)
+    if args.index is not None:
+        mall = mall_db[args.index]
+    else:
+        mall = random.choice(mall_db)
     head_text = open(HEAD,"r").read()
     p.image(LOGO)
     p.set(align="center")
@@ -46,6 +56,12 @@ if __name__ == '__main__':
     p.set(align="right")
     p.text("-{}\n".format(mall['memory_author']))
     p.text("="*WIDTH)
-    p.set(align="left")
+    p.text("\n")
+    p.set(align="center")
+    p.set(align="center")
+    p.text(open(FOOT,"r").read())
+    p.qr(QRLINK,size=10)
+    p.text("\n")
+    p.set(text_type="normal")
     p.cut()
     
